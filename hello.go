dd9372db261ce1c6b37d384f853d16030cfc0c5c
akshay <kinhikar.akshay32@gmail.com>
2020-02-15 12:34:30 +0530

    datatype : boolean

--- hello.go
+++ hello.go
@@ -3,10 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	// print stmt
 	fmt.Println("Hello, world.")
 
+	// variable declaration, initialization
 	var a int32
 
 	a = 15
 	fmt.Println("value for a =", a)
+
+	// dataType
+	// boolean dataType
+	var b bool
+
+	b = true
+
+	fmt.Println("value of b= ", b)
+
 }
